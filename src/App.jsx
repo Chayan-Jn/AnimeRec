0a6d5fbd7510b28bf65f3641a30d539b4e3ea3ed
chayan <chayanj39@gmail.com>
2025-07-16 13:07:24 +0530

    Anime recommender's frontend

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react"
+import './app.css'
+const App = ()=>{
+  const [anime,setAnime] = useState("")
+  const [recom,setRecom] = useState(null)
+  const [searched,setSearched] = useState(false)
+  async function handleSubmit(e){
+    e.preventDefault();
+    setRecom(null) // set recom to null so that the searching for anime comes up
+    setSearched(true) // set this to true so that i know when to show the searching block.. 
+    if(anime.trim()){
+      try{
+        const response = await fetch(`https://animerecombackend.onrender.com/?anime=${anime}`,{method:"GET"})
+        const data = await response.json()
+        setRecom(data)
+      }
+      catch(err){
+        console.log("Error fetching recommendation ",err)
+      }
+    }
+  }
+  return <div className="main">
+    <form onSubmit={handleSubmit}
+    className="form">
+      <input onChange={e=>{
+        setSearched(false)
+        setAnime(e.target.value)
+      }} value={anime}/>
+      <button type="submit">RECOMMEND</button>
+    </form>
+    {recom && 
+      <div>
+        <div className="to-recom">
+          Recommendation for {anime} are
+        </div>
+        {Object.entries(recom).map(([key,value])=>{
+          return <div className="recom-container" key={value}>
+            <span className="key">{key} </span>
+            <span className="value">{value}</span>
+
+          </div>
+        })}
+      </div>}
+      {anime && !recom && searched && 
+      <div>
+        <div className="searching">
+          Searching for the best animes
+        </div>
+      </div>}
+  </div>
+}
+export default App
